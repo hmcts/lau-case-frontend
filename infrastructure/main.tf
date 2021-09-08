7912ba338cb9fcdb47996a1ad2dfc5b3c74eb763
Bradley Wadsworth <bradley.wadsworth@hmcts.net>
2021-09-08 16:04:11 +0100

    LAU-107 Add missing subnet

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,6 +6,12 @@ locals {
   vaultName = "${var.product}-${var.env}"
 }
 
+data "azurerm_subnet" "core_infra_redis_subnet" {
+  name                 = "core-infra-subnet-1-${var.env}"
+  virtual_network_name = "core-infra-vnet-${var.env}"
+  resource_group_name = "core-infra-${var.env}"
+}
+
 module "lau-case-frontend-session-storage" {
   source   = "git@github.com:hmcts/cnp-module-redis?ref=azurermv2"
   product  = "${var.product}-${var.component}-session-storage"
