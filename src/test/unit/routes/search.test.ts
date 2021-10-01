ab590d90b01887f00969f9c6d2014fe5c39edf85
bradley-hmcts <61189290+bradley-hmcts@users.noreply.github.com>
2021-10-01 10:11:53 +0100

    Update search.test.ts

--- src/test/unit/routes/search.test.ts
+++ src/test/unit/routes/search.test.ts
@@ -8,6 +8,6 @@ describe('Search Route', () => {
   it('Calls the Search Controller post method', async () => {
     const res = await request(app).post('/search');
     expect(res.header['content-type']).toBe('text/plain; charset=utf-8');
-    expect(res.statusCode).toBe(200);
+    expect(res.statusCode).toBe(302);
   });
 });
