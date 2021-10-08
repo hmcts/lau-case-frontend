bba644b9c931fc665778607e27be2cc3297636f3
Bradley Wadsworth <bradley.wadsworth@hmcts.net>
2021-10-08 12:50:35 +0100

    LAU-130 Force TLSv1_2 method for dev server

--- src/main/server.ts
+++ src/main/server.ts
@@ -12,7 +12,7 @@ const port: number = parseInt(process.env.PORT, 10) || 4000;
 if (app.locals.ENV === 'development') {
   const sslDirectory = path.join(__dirname, 'resources', 'localhost-ssl');
   const sslOptions: https.ServerOptions = {
-    minVersion: 'TLSv1.2',
+    secureProtocol: 'TLSv1_2_method',
     cert: fs.readFileSync(path.join(sslDirectory, 'localhost.crt')),
     key: fs.readFileSync(path.join(sslDirectory, 'localhost.key')),
   };
